b7e46613db202e0bec1c7bcf889a46b47abd372d
Natsuo <1655659+natsuo@users.noreply.github.com>
2018-07-17 01:17:36 +0200

    Adds XML comments to methods

--- Miqo.Config/Configuration.cs
+++ Miqo.Config/Configuration.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
 namespace Miqo.Config {
+	/// <summary>
+	/// A data type containing metadata about a configuration file.
+	/// </summary>
 	public class Configuration {
 		public string Raw { get; set; }
 		public object Parsed { get; set; }
--- Miqo.Config/ConfigurationItem.cs
+++ Miqo.Config/ConfigurationItem.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
 namespace Miqo.Config {
+	/// <summary>
+	/// A data type containing metadata about a configuration item.
+	/// </summary>
 	internal class ConfigurationItem {
 		public string Data { get; set; }
 		public DateTime LastUpdated { get; set; }
