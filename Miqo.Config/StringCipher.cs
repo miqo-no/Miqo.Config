4a01692b41cbb5c9d3f814a3dfdee51e606dcda5
Ronny Natsuo <1655659+natsuo@users.noreply.github.com>
2018-10-09 18:28:43 +0200

    fix(StringCipher): base64 encoded ciphers
    BREAKING CHANGE:
    
    AES encrypted information was stored as hex encoded strings instead of base64. This resulted in the strings being larger than necessary.
    
    Miqo.Config can still load hex encoded strings, but they will be saved as base64.

--- Miqo.Config/StringCipher.cs
+++ Miqo.Config/StringCipher.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Text;
-using System.Security.Cryptography;
-using System.IO;
 
 namespace Miqo.Config {
 	public static class StringCipher {
@@ -10,12 +8,18 @@ namespace Miqo.Config {
 				new Ckode.Encryption.AESWithPassword()
 					.Encrypt(text, Encoding.UTF8, passPhrase);
 
-			return cipher.ToHex();
+			return Convert.ToBase64String(cipher);
 		}
 
 		public static string DecryptString(string cipher, string passPhrase) {
+			// Check if cipher is stored as a hex encoded string instead of a base64 string.
+			var bytes = StringContainsOnlyHex(cipher)
+				? cipher.HexToBytes()
+				: Convert.FromBase64String(cipher);
+
 			return new Ckode.Encryption.AESWithPassword()
-				.Decrypt(cipher.HexToBytes(), Encoding.UTF8, passPhrase);
+				.Decrypt(bytes, Encoding.UTF8, passPhrase);
+
 		}
 
 		public static string CreateRandomKey() {
@@ -23,5 +27,8 @@ namespace Miqo.Config {
 
 			return key.ToHex();
 		}
+
+		public static bool StringContainsOnlyHex(string test)
+			=> System.Text.RegularExpressions.Regex.IsMatch(test, @"\A\b[0-9a-fA-F]+\b\Z");
 	}
 }
